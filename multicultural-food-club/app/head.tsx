ec3967be3a8dd47395d2a47c66f06788f4acc075
Corina Conklin <corina0615@gmail.com>
2023-10-05 20:08:23 -0500

    added members page

--- multicultural-food-club/app/head.tsx
+++ multicultural-food-club/app/head.tsx
@@ -8,8 +8,9 @@ export default function Head() {
         <ul>
           <li><a href='./'>Home</a></li>
           <li><a href='./reviews'>Reviews</a></li>
-          <li><a href='./newsletter'>Newsletter</a></li>
           <li><a href='./restaurants'>Restaurants</a></li>
+          <li><a href='./members'>Members</a></li>
+          <li><a href='./newsletter'>Newsletter</a></li>
           <li><a href='./contact'>Contact</a></li>
           <li><a href='./about'>About Us</a></li>
           <li>Search</li>
---
+++ multicultural-food-club/app/members/page.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function Members() {
+    return (
+        <React.Fragment>
+            <h2>Members</h2>
+            <p>moldy pizza</p>
+        </React.Fragment>
+    )
+}
