eab1f76edc790b340761b13e050c9abc631631d4
ManChat1 <mananchat03@gmail.com>
2023-10-12 20:19:08 -0500

    Update page.tsx
    added more contact information, nothing yet is serious.

--- multicultural-food-club/app/contact/page.tsx
+++ multicultural-food-club/app/contact/page.tsx
@@ -3,8 +3,18 @@ import React from "react"
 export default function Contact() {
     return (
         <React.Fragment>
-            <h2>Contact</h2>
-            <p>yada yada</p>
+            <h1>Contacts</h1>
+            <h2>Email:</h2>
+            <p>loremipsum@gmail.com</p>
+            <h2>Social Media:</h2>
+            <h3>Instagram:</h3>
+            <p>loremipsum</p>
+            <h3>Snapchat:</h3>
+            <p>loremipsum</p>
+            <h3>Google Plus:</h3>
+            <p>...no</p>
+            <h2>Phone Number:</h2>
+            <p>123-456-7890</p>
         </React.Fragment>
     )
 }
