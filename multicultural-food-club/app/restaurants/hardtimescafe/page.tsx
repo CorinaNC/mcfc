89f3f4c711637185dc8eea0468b3c5706cc2bda3
Corina Conklin <corina0615@gmail.com>
2023-10-13 10:52:12 -0500

    security

--- multicultural-food-club/app/restaurants/hardtimescafe/page.tsx
+++ multicultural-food-club/app/restaurants/hardtimescafe/page.tsx
@@ -7,7 +7,7 @@ export default function About() {
             <Head />
             <div class="top">
                 <h1>Hard Times Cafe</h1>
-                <iframe width="600" height="450" loading="lazy" allowfullscreen src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJIxHJx0Mts1IRlGMFZ_q61xo&key=AIzaSyA8TjgXV4ZjwmrgIS134ONdfVwQoWCdLSQ"></iframe>
+                <iframe width="600" height="450" loading="lazy" allowfullscreen src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJIxHJx0Mts1IRlGMFZ_q61xo&key="></iframe>
             </div>
         </React.Fragment>
     )
