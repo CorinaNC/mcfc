0446ffef22dea1d69cc2c9603b64d40b036b3528
Corina Conklin <corina0615@gmail.com>
2023-10-26 19:38:12 -0500

    privacy

--- multicultural-food-club/app/restaurants/hardtimescafe/page.tsx
+++ multicultural-food-club/app/restaurants/hardtimescafe/page.tsx
@@ -1,6 +1,7 @@
 import React from "react"
 import Head from "@/components/head"
-require('dotenv').config();
+
+const apiKey = process.env.REACT_APP_GOOGLE_MAPS_API_KEY;
 
 export default function About() {
     return (
@@ -8,7 +9,7 @@ export default function About() {
             <Head />
             <div class="top">
                 <h1>Hard Times Cafe</h1>
-                <iframe width="600" height="450" loading="lazy" allowfullscreen src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJIxHJx0Mts1IRlGMFZ_q61xo&key="></iframe>
+                <iframe width="600" height="450" loading="lazy" allowfullscreen src={`https://www.google.com/maps/embed/v1/place?q=place_id:ChIJA0QhW0Ets1IRNP8Kas5fdBY&key=${apiKey}`}></iframe>
             </div>
         </React.Fragment>
     )
